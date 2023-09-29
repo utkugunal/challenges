1bf931f7cf207300cb81eb382d0e656e2ee2cd1d
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-09-29 16:21:00 +0200

    finish the booleans challenge

--- js-conditions-booleans/js-conditions-and-booleans_booleans/js/index.js
+++ js-conditions-booleans/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
 console.log(Boolean("Try it out"));
+
+console.log("1. ", Boolean(2 == "2"));
+console.log("2. ", Boolean(2 === "2"));
+console.log("3. ", Boolean(2 === 2));
+console.log("4. ", Boolean("Hello world"));
+console.log("5. ", Boolean(""));
+console.log("6. ", Boolean(true));
+console.log("7. ", Boolean(0));
+console.log("8. ", Boolean(-50));
+console.log("9. ", Boolean(50));
+console.log("10. ", Boolean(5 > 1));
+console.log("11. ", Boolean(1 > 10));
+console.log("12. ", Boolean(false));
