d02f6d4a2d1fe59be735ad2bd37c215e6cf2f2b8
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-06 13:36:13 +0200

    finish the switch-values challenge

--- js-inputs-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -17,10 +17,22 @@ const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 
+const secondInput = document.querySelector('[data-js="second-input"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
+
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  const oldFirstInput = firstInput.value;
+  const oldSecondInput = secondInput.value;
+  firstInput.value = oldSecondInput;
+  secondInput.value = oldFirstInput;
 });
