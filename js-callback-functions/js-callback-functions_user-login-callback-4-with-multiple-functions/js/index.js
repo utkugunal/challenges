6e75c7d742551110012fac4a649a1c95988bcf91
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-11 17:23:13 +0200

    start the multiple callback challenge

--- js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ js-callback-functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -1,11 +1,25 @@
 console.clear();
 
+//handleUserlogin is our higher order function. we define a function with 3 parameters, and 2 of them are already functions.
 function handleUserLogin(onSuccess, onError, userName) {
   if (userName === "Jane Doe") {
-    onSuccess(userName, "admin");
+    onSuccess(userName, "admin"); // this function has 2 arguments.
   } else {
-    onError(`Unknown user "${userName}"`);
+    onError(`Unknown user "${userName}"`); // this function has 1 argument. this argument will be used as whole, whenever it is called later.
   }
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+function showErrorMessage(errorMessage) {
+  //
+  console.log(`Login error: ${errorMessage}`);
+}
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
+
+handleUserLogin(showWelcomeMessage, showErrorMessage, "John Doe");
