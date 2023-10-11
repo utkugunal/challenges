6748ad481819e3a2b2c3d417e365b4ec61d6f84f
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-11 16:00:33 +0200

    finish the one-argument challenge

--- js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,13 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName) {
+  console.log("Welcome Jane Doe! You are logged in now.");
+}
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {
+  showWelcomeMessage();
+});
