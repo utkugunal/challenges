6badfca7dae3b06cdd3bd40edcf5d84f6ff508c7
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-12 12:23:38 +0200

    finish the for-each challenge

--- js-array-methods/js-array-methods_for-each/js/index.js
+++ js-array-methods/js-array-methods_for-each/js/index.js
@@ -19,4 +19,18 @@ const colors = [
   "#A5A5A5",
 ];
 
-colors.forEach();
+colors.forEach((color) => {
+  const colorElement = document.createElement("div");
+  colorElement.classList.add("color-box");
+  colorElement.style.backgroundColor = color;
+  document.body.append(colorElement);
+});
+
+function renderColorBox(color) {
+  const colorElement = document.createElement("div");
+  colorElement.classList.add("color-box");
+  colorElement.style.backgroundColor = color;
+  document.body.append(colorElement);
+}
+
+colors.forEach(renderColorBox);
