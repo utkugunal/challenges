121144e0bddc4323d3cf6f06a908643f6ab91652
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-15 22:08:38 +0200

    finish the stars challenge

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,12 +2,23 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let i = 1; i < 6; i++) {
+    let img = document.createElement("img");
+    if (i <= filledStars) {
+      img.setAttribute("src", "/assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "/assets/star-empty.svg");
+    }
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(img);
+  }
   //--^-- your code here --^--
 }
 
