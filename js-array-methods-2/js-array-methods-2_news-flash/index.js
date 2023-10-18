6a55e82405541f0e3ef32c50cda376adf67404e4
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-18 16:37:51 +0200

    finish the news-flash challenge

--- js-array-methods-2/js-array-methods-2_news-flash/index.js
+++ js-array-methods-2/js-array-methods-2_news-flash/index.js
@@ -5,12 +5,22 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((article) =>
+  article.categories.includes("politics")
+);
 
 // Part 2 - start here
 const sortedNews = filteredNews;
+
+sortedNews.sort((a, b) => {
+  if (a.body.length < b.body.length) {
+    return -1;
+  }
+  if (a.body.length > b.body.length) {
+    return 1;
+  }
+  return 0;
+});
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
