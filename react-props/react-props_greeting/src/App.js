bb328309a269b7ad36cda43135da479e66bd5785
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-25 16:04:03 +0200

    finish greeting

--- react-props/react-props_greeting/src/App.js
+++ react-props/react-props_greeting/src/App.js
@@ -1,5 +1,12 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Jan" />;
+}
+
+function Greeting({ name }) {
+  if (name == "Jan" || name == "Marcell") {
+    return <h1>Hello, Coach!</h1>;
+  }
+  return <h1>Change the coach</h1>;
 }
