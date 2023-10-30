f3df0944bcfa5727028b5a2d0a100f6147d3c327
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-30 15:40:40 +0100

    finish state-form

--- react-state-3/react-state-3_journal-app-form/src/components/EntryForm/index.js
+++ react-state-3/react-state-3_journal-app-form/src/components/EntryForm/index.js
@@ -1,9 +1,19 @@
 import "./EntryForm.css";
 import Button from "../Button";
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    const newObject = { motto: data.motto, notes: data.notes };
+    onAddEntry(newObject);
+    event.target.reset();
+  }
+
   return (
-    <form className="entry-form">
+    <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">New Entry</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
