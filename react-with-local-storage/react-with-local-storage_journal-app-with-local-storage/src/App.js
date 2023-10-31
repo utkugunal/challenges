02222e3d5edc56f41577008b6206e7f3ff182b07
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-10-31 11:34:47 +0100

    finish journal-local-storage

--- react-with-local-storage/react-with-local-storage_journal-app-with-local-storage/src/App.js
+++ react-with-local-storage/react-with-local-storage_journal-app-with-local-storage/src/App.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { uid } from "uid";
+import { useState } from "react";
 
 const initialEntries = [
   {
@@ -37,8 +38,12 @@ const initialEntries = [
 ];
 
 function App() {
-  const [entries, setEntries] = useState(initialEntries);
-  const [filter, setFilter] = useState("all"); // "all" or "favorites"
+  const [entries, setEntries] = useLocalStorageState("journalEntries", {
+    defaultValue: initialEntries,
+  });
+  const [filter, setFilter] = useLocalStorageState("filter", {
+    defaultValue: "all",
+  }); // "all" or "favorites"
 
   function handleAddEntry(newEntry) {
     const date = new Date().toLocaleDateString("en-us", {
