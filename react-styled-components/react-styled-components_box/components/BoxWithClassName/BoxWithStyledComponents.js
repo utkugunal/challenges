f365c28b482786cfeebfdb2da329f74f48270630
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-11-11 16:03:40 +0100

    finish styled-box without bonuses

---
+++ react-styled-components/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default function BoxWithStyledComponents({ isBlack }) {
+  return <StyledBox $isBlack={isBlack}></StyledBox>;
+}
+
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 2rem;
+  background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
+  &:hover {
+    background-color: red;
+  }
+`;
