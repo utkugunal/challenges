8a3f896aad77a7f602b6452d961f2b1cbed9f9ec
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-11-20 17:02:20 +0100

    finish backend-chance

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,19 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  const age = chance.age();
+  const name = chance.name();
+  const profession = chance.profession();
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Go back to the main page using only '/' in the URL");
+  }
+});
