9f64b1c2d9d95677d08ce36e7d661dd8fdd3d65f
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-11-21 15:33:59 +0100

    finish backend-random

--- backend-api/backend-api-routes_random-character/pages/index.js
+++ backend-api/backend-api-routes_random-character/pages/index.js
@@ -4,5 +4,10 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function HomePage() {
   const { data } = useSWR("/api/random-character", fetcher);
-  return <h1>Hello from Next.js! Here is your data: {data}</h1>;
+
+  if (!data) {
+    return;
+  }
+
+  return <h1>Hello from Next.js! Here is your data: {data.age}</h1>;
 }
