be982ddfe66c49f769951db33f355252e0e63fad
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-11-23 17:55:40 +0100

    finish the update part

---
+++ backend-update/backend-update-and-delete_products/components/Product/index.js
@@ -0,0 +1,70 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+
+export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  // why does it accept an empty string when I want to edit a fish, even though the schema says, it is required.
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    event.target.reset();
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  // if (!data) {
+  //   return;
+  // }
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </button>
+      <ProductCard>
+        <h2>{data.name}</h2>
+        <p>Description: {data.description}</p>
+        <p>
+          Price: {data.price} {data.currency}
+        </p>
+        {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+        <StyledLink href="/">Back to all</StyledLink>
+      </ProductCard>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} />}
+    </>
+  );
+}
